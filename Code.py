36a2394ce8d0e1012ef073ca2319498fb06d7554
OliverMMiller <124292008+OliverMMiller@users.noreply.github.com>
2023-12-04 16:32:54 -0700

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,11 +1,18 @@
-def coeficentsToPolynomial(imput):
-    listIn = imput.split(",")
-    strOut = ""
-    degree = len(listIn)
-    for i in listIn:
-        strOut = strOut+i+"x^"+str(degree)+"+"
-        degree -= 1
-    strOut = strOut.strip("+")
-    return (strOut)
+def letterPermutations(lettersin):
+  import math
+  dict = {}
+  i = ""
+  divider = 1
+  lettersin = str(lettersin)
+  for i in lettersin.lower():
+    if i in dict:
+      dict[i] += 1
+    else:
+      dict.update({i : 1})
+     
+  for i in dict:
+    divider *= math.factorial(dict[i])
+  return math.factorial(len(lettersin))/(divider)
 
-print(coeficentsToPolynomial(input("text: ")))
+
+print(letterPermutations(input("input letters: ")))
